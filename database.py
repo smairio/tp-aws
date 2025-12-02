4ca92394c61fc6b187fafefd485900d11d87587d
khalil smairi <smairio98@gmail.com>
2025-12-02 13:43:20 +0100

    first commit

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user_name:password@endpoint/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Beastmode1998@database.internal.local/myappdb"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
