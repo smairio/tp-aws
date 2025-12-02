088f8425e574cccc4a2e67dac6b6be9f2608a25c
khalil smairi <smairio98@gmail.com>
2025-12-02 14:29:05 +0100

    first

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Beastmode1998@database.internal.local/myappdb"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Beastmode1998@myappdb.cf6iaecuodcj.us-east-1.rds.amazonaws.com:5432/myappdb"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
